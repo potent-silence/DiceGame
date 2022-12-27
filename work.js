1b80e449bd0c6a22faccd9ae2f3114cd6a2f81e4
potent-silence <75715464+potent-silence@users.noreply.github.com>
2022-12-27 13:55:03 +0530

    Dice Game V2

--- work.js
+++ work.js
@@ -3,20 +3,24 @@ var p2=prompt("Name of Player 2: ");
 document.querySelector(".p1 ").textContent=p1;
 document.querySelector(".p2 ").textContent=p2;
 
-var d1=document.querySelector(".img1 "),r1=Math.random();
-var d2=document.querySelector(".img2 "),r2=Math.random();
-r1*=6;r2*=6;
-r1=Math.floor(r1);r1++;
-r2=Math.floor(r2);r2++;
-d1.setAttribute("src","images/dice"+r1+".png");
-d2.setAttribute("src","images/dice"+r2+".png");
-var judgement=document.querySelector(".judge ");
-if(r1===r2){
-    judgement.textContent="Try Again 😊! ";
+function again(){
+    var d1=document.querySelector(".img1 "),r1=Math.random();
+    var d2=document.querySelector(".img2 "),r2=Math.random();
+    r1*=6;r2*=6;
+    r1=Math.floor(r1);r1++;
+    r2=Math.floor(r2);r2++;
+    d1.setAttribute("src","images/dice"+r1+".png");
+    d2.setAttribute("src","images/dice"+r2+".png");
+    var judgement=document.querySelector(".judge ");
+    if(r1===r2){
+        judgement.textContent="Try Again 😊! ";
+    }
+    else if(r1>r2){
+        judgement.textContent="🚩 "+p1+" Wins!";
+    }
+    else{
+        judgement.textContent=p2+" Wins! 🚩";
+    }
 }
-else if(r1>r2){
-    judgement.textContent="🚩 "+p1+" Wins!";
-}
-else{
-    judgement.textContent=p2+" Wins! 🚩";
-}
+
+document.querySelector("button").addEventListener("click",again);
